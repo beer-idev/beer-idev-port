e84dc575df99fbc3a97433ac4fadb49f6fda5fe5
beertg <102638818+beertg-dev@users.noreply.github.com>
2022-09-04 22:16:30 +0700

    commit update footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <section className="bg-gray-100 dark:bg-black shadow-xl shadow-slate-100	">
       <div className="max-w-screen-xl px-4 py-8 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
-        <div className="flex justify-center mt-8 space-x-6">
+        <div className="flex justify-center mt-8 space-x-2 lg:space-x-6">
           <IconContact />
         </div>
         <p className="mt-8 text-base leading-6 text-center text-gray-400">
